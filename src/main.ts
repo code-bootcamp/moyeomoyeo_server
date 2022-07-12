30eeb0a5af656565f9fc8ff1ea5a1a1746b52b86
1nhou <inhouuuuu@gmail.com>
2022-07-12 17:34:24 +0900

    [C] Board Delete Function / [D] package.json(body-parse)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress());
+  app.use(cookieParser());
+  app.enableCors();
 
   await app.listen(3000);
 }
