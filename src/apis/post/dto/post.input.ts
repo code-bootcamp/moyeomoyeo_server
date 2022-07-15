d479f013cc92e32398dfd23f04eb8d6c60382a47
1nhou <inhouuuuu@gmail.com>
2022-07-15 20:16:02 +0900

    [U]docker mysql

---
+++ src/apis/post/dto/post.input.ts
@@ -0,0 +1,50 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Category } from 'src/apis/category/entities/category.entity';
+import { User } from 'src/apis/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class PostInput {
+  @Column()
+  @Field(() => String)
+  title: string;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  writer: User;
+
+  @Column()
+  @Field(() => String)
+  fileSrc: string;
+
+  @Column()
+  @Field(() => String)
+  headerImgSrc: string;
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  uploadedAt: Date;
+
+  @UpdateDateColumn()
+  @Field(() => Date)
+  editedAt: Date;
+
+  @Column({ default: 0 })
+  @Field(() => Int)
+  viewCount: number;
+
+  @JoinTable()
+  @ManyToMany(() => Category, (categories) => categories.posts)
+  @Field(() => [Category])
+  categories: Category[];
+}
